d9d6e7365b4063526d1f5d7da4806fd766802215
Techie-Mahesh <maheshTech1205@gmail.com>
2025-08-06 18:07:26 +0530

    Header,footer components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
